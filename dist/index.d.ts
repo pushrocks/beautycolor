9043933be5cb2678502d87b288a8b3caf4f2c9e1
Phil Kunz <gitlab@philkunz.com>
2017-08-16 12:14:49 +0200

    fix indention

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,7 +2,7 @@ import 'typings-global';
 /**
  * all the color names that are available for proper xterm translation
  */
-export declare type TColorName = 'black' | 'black' | 'blue' | 'brown' | 'cyan' | 'green' | 'orange' | 'pink' | 'red' | 'white';
+export declare type TColorName = 'black' | 'blue' | 'brown' | 'cyan' | 'green' | 'orange' | 'pink' | 'red' | 'white';
 export interface IRGB {
     r: number;
     b: number;
